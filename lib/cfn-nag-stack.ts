123b43f92b4bee86b96876e70fb504f218ff28e8
Ross Rhodes <trh@trustpilot.com>
2021-08-25 11:45:17 +0100

    initialise rds stack for cfn nag

--- lib/cfn-nag-stack.ts
+++ lib/cfn-nag-stack.ts
@@ -1,9 +1,20 @@
+import { Vpc } from '@aws-cdk/aws-ec2';
+import { DatabaseCluster, DatabaseClusterEngine } from '@aws-cdk/aws-rds';
 import * as cdk from '@aws-cdk/core';
 
 export class CfnNagStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = Vpc.fromLookup(this, "Vpc", {
+      vpcId: String(process.env.VPC_ID),
+    });
+
+    new DatabaseCluster(this, "DatabaseCluster", {
+      engine: DatabaseClusterEngine.AURORA,
+      instanceProps: {
+        vpc,
+      },
+    });
   }
 }
